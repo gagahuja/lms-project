a615020c3ab3d034d1a7ee00b321b896762c19e8
Gagan <gagahuja1975@gmail.com>
2026-03-31 00:28:10 +0530

    fixed 500 error (safe loops + prefetch)

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*=vz9ecxrodgope#6&iv+jees04q7#$79%zqx@tg(^@2xqw7(q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
