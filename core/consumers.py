ba93a929ac83ca70a5164e052908d2e25b331f66
Gagan <gagahuja1975@gmail.com>
2026-04-24 14:09:59 +0530

    added raise hand system UI + backend

--- core/consumers.py
+++ core/consumers.py
@@ -23,19 +23,52 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         data = json.loads(text_data)
 
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                "type": "chat_message",
-                "message": data.get("message"),
-                "user": self.scope["user"].username,
-                "msg_type": data.get("type", "chat")
-            }   
-        )
+        msg_type = data.get("type")
+
+        if msg_type == "mute_user":
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    "type": "mute_command",
+                    "target": data.get("target"),
+                }
+            )
+        elif msg_type == "raise_hand":
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    "type": "raise_hand_event",
+                    "user": self.scope["user"].username,
+                }
+            )
+        else:
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    "type": "chat_message",
+                    "message": data.get("message"),
+                    "user": self.scope["user"].username,
+                    "msg_type": "chat"
+                }
+            )
+        
+
+
+    async def mute_command(self, event):
+        await self.send(text_data=json.dumps({
+            "type": "mute_user",
+            "target": event["target"]
+        }))
 
     async def chat_message(self, event):
         await self.send(text_data=json.dumps({
             "message": event["message"],
             "user": event["user"],
             "type": event.get("msg_type", "chat")
+        }))
+
+    async def raise_hand_event(self, event):
+        await self.send(text_data=json.dumps({
+            "type": "raise_hand",
+            "user": event["user"]
         }))
