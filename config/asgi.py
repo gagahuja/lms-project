fbcbc6e980152edf9e1f304c6f05c2c8339fd79d
Gagan <gagahuja1975@gmail.com>
2026-04-27 12:45:43 +0530

    Fix 502 with correct ASGI + Daphne setup

--- config/asgi.py
+++ config/asgi.py
@@ -8,19 +8,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-import core.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+django_asgi_app = get_asgi_application()
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import core.routing
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            core.routing.websocket_urlpatterns
-        )
+        URLRouter(core.routing.websocket_urlpatterns)
     ),
 })
