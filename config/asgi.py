384d25b317c3938b325805a73793920300b4c241
Gagan <gagahuja1975@gmail.com>
2026-04-28 12:36:52 +0530

    Fix 502 - revert ASGI to stable

--- config/asgi.py
+++ config/asgi.py
@@ -8,23 +8,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-import django
-
-print("🔥 ASGI LOADED")
-
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from core import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django.setup()
 
-print("🔥 ROUTING LOADED")
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-
-    "websocket": URLRouter(
-        routing.websocket_urlpatterns
-    ),
-})
+application = get_asgi_application()
